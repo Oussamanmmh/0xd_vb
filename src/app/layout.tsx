5b4ff570a28a3bf7b3fa656724ad7f43c84156bf
oussamanmh <mo_nemamcha@esi.dz>
2025-06-28 13:43:09 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import LanguageSelector from "@/components/mode/lang";
-import ThemeToggle from "@/components/mode/theme";
 
 const poppins = Poppins({
   variable: "--font-poppins",
